5e397fbc9a36adfe86f4e4397e85032e9fab62ed
antonpandi <anton.jonsson@gmail.com>
2022-11-14 15:24:47 +0100

    Added admin and picture

---
+++ src/Entity/image.php
@@ -0,0 +1,133 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Image{
+    /**
+     * @ORM\Id()
+     * @ORM\GenerateValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    public $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    public $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    public $size;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    public $date;
+
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of size
+     */ 
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * Set the value of size
+     *
+     * @return  self
+     */ 
+    public function setSize($size)
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of date
+     */ 
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the value of date
+     *
+     * @return  self
+     */ 
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
